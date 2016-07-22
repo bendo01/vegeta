8953efe868504c815890bf7e49a9c269506deacf
BennyLEP <bendo01@gmail.com>
2016-07-23 01:54:00 +0800

    change to version 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 'use strict';
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
-var concat = require("gulp-concat");
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var notify = require("gulp-notify");
+var notify = require('gulp-notify');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var postcss = require('gulp-postcss');
+var vfs = require('vinyl-fs');
+var converter = require('sass-convert');
 
 var config = {
    sassPath: './src/scss',
   npmDir: './node_modules'
-}
+};
+
+gulp.task('convert-to-scss', function () {
+  return gulp.src(config.npmDir + '/bulma/sass/**/*.+(sass|scss)')
+    .pipe(converter({
+      from: 'sass',
+      to: 'scss',
+    }))
+    .pipe(rename(function(path){
+      path.extname = '.scss';
+    }))
+    .pipe(vfs.dest('./src/scss'));
+});
 
 gulp.task('compile-scss', function () {
   gulp.src('vegeta.scss')
